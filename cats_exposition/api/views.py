fabaceed1413bfcdacd80f81f6ebd503bba82571
bissaliev <bissaliev21@gmail.com>
2024-10-01 18:31:13 +0400

    Создал пермишен OwnerOrReadOnly и добавил во вьюсет CatViewSet

--- cats_exposition/api/views.py
+++ cats_exposition/api/views.py
@@ -1,12 +1,14 @@
 from cats.models import Breed, Cat
 from rest_framework.viewsets import ModelViewSet
 
+from .permissions import OwnerOrReadOnly
 from .serializers import BreedSerializer, CatSerializer
 
 
 class CatViewSet(ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = [OwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
