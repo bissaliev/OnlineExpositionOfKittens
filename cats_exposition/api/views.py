b7a49dee7c987117cb79c9aaf35cce3071fdaacd
bissaliev <bissaliev21@gmail.com>
2024-10-03 06:53:14 +0400

    Добавил вьюсет RatingViewSet

--- cats_exposition/api/views.py
+++ cats_exposition/api/views.py
@@ -1,10 +1,17 @@
-from cats.models import Breed, Cat
+from cats.models import Breed, Cat, Rating
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import SAFE_METHODS
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import CatFilter
-from .permissions import OwnerOrReadOnly
-from .serializers import BreedSerializer, CatSerializer
+from .permissions import AuthorOrReadOnly, OwnerOrReadOnly
+from .serializers import (
+    BreedSerializer,
+    CatCreateSerializer,
+    CatSerializer,
+    RatingSerializer,
+)
 
 
 class CatViewSet(ModelViewSet):
@@ -19,9 +26,28 @@ class CatViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get_serializer_class(self):
+        if self.request.method not in SAFE_METHODS:
+            return CatCreateSerializer
+        return super().get_serializer_class()
+
 
 class BreedViewSet(ModelViewSet):
     """ViewSet для работы с породами котиков."""
 
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
+
+
+class RatingViewSet(ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        cat = get_object_or_404(Cat, pk=self.kwargs.get("cat_id"))
+        return super().get_queryset().filter(cat=cat)
+
+    def perform_create(self, serializer):
+        cat = get_object_or_404(Cat, pk=self.kwargs.get("cat_id"))
+        serializer.save(user=self.request.user, cat=cat)
