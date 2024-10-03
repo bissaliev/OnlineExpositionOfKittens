baba50f6ba0c6b3831e23fcda490e040ec386069
bissaliev <bissaliev21@gmail.com>
2024-10-03 06:50:25 +0400

    Добавил модель Rating

--- cats_exposition/cats/models.py
+++ cats_exposition/cats/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from .validators import validate_birth_date
@@ -55,3 +57,44 @@ class Cat(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Rating(models.Model):
+    """Рейтинг котиков."""
+
+    cat = models.ForeignKey(
+        Cat, on_delete=models.CASCADE, related_name="ratings"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="ratings"
+    )
+    score = models.PositiveSmallIntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(1, "Оценка должна быть в диапазоне от 1 до 5"),
+            MaxValueValidator(5, "Оценка должна быть в диапазоне от 1 до 5"),
+        ],
+    )
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["cat", "user"], name="unique_rating"
+            )
+        ]
+
+    def __str__(self):
+        return (
+            f"Пользователь {self.user.username} поставил "
+            f"котику {self.cat.name} оценку {self.score}"
+        )
+
+    def clean(self) -> None:
+        if self.user == self.cat.owner:
+            raise ValidationError("Нельзя оценивать своего котика!")
+
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        super().save(*args, **kwargs)
