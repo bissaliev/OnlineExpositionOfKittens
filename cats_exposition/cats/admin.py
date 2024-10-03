639e7d5803f56069805143e3c330a80477fb33ab
bissaliev <bissaliev21@gmail.com>
2024-10-03 06:54:54 +0400

    Добавил модель Rating в админку

--- cats_exposition/cats/admin.py
+++ cats_exposition/cats/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Breed, Cat
+from .models import Breed, Cat, Rating
 
 admin.site.register(Cat)
 admin.site.register(Breed)
+admin.site.register(Rating)
