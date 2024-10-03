87f79c73db8c7e9f7ae7ec03b6ba7c0df624b932
bissaliev <bissaliev21@gmail.com>
2024-10-03 16:56:49 +0400

    Добавил фильтрацию котиков по рейтингу

--- cats_exposition/api/filters.py
+++ cats_exposition/api/filters.py
@@ -7,7 +7,11 @@ class CatFilter(filters.FilterSet):
 
     breed = filters.CharFilter(field_name="breed__name")
     owner = filters.CharFilter(field_name="owner__username")
+    rating = filters.NumberFilter(method="filter_by_rating")
 
     class Meta:
         model = Cat
-        fields = ("breed", "color", "owner")
+        fields = ("breed", "color", "owner", "rating")
+
+    def filter_by_rating(self, queryset, name, value):
+        return queryset.filter(rating_avg__gte=value)
