c1da1d52c0ee920972c8c834ed76d734759ec5d4
bissaliev <bissaliev21@gmail.com>
2024-10-04 19:43:01 +0400

    Изменил функцию calculating_age_in_months

--- cats_exposition/api/utils.py
+++ cats_exposition/api/utils.py
@@ -1,12 +1,12 @@
 from datetime import date
 
 
-def calculating_age_in_months(date: date) -> int:
+def calculating_age_in_months(birth_date: date) -> int:
     """Функция вычисляет возраст в месяцах."""
     today = date.today()
-    years_diff = today.year - date.year
-    months_diff = today.month - date.month
+    years_diff = today.year - birth_date.year
+    months_diff = today.month - birth_date.month
     total_month = years_diff * 12 + months_diff
-    if today.day < date.day:
+    if today.day < birth_date.day:
         total_month -= 1
     return total_month
